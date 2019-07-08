f64c25dead4c5b157b3b21fbcfacb684f8553f9c
Ben Parisot <me@benparisot.com>
2019-07-08 14:27:53 -0700

    rewrite findGcdPair to be a recursive function

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ console.log('hello ben');
 const arr1 = [28, 16, 44, 124, 100];
 const arr2 = [4, 3, 19, 33, 26];
 const arr3 = [55, 235, 21, 8, 43, 88];
+const arr4 = [1, 2, 3];
+const arr5 = [2, 4, 6, 8];
 
 function findGcdArray(num, array) {
     function findGcdPair(x, y) {
         x = Math.abs(x);
         y = Math.abs(y);
-        while(y) {
-            const t = y;
-            y = x % y;
-            x = t;
-        } 
-        return x;
+        if(x === 0) return y;
+        return findGcdPair(y % x, x);
     }
     let a;
     let b;
@@ -31,3 +29,5 @@ function findGcdArray(num, array) {
 console.log(findGcdArray(5, arr1));
 console.log(findGcdArray(5, arr2));
 console.log(findGcdArray(6, arr3));
+console.log(findGcdArray(3, arr4));
+console.log(findGcdArray(4, arr5));
